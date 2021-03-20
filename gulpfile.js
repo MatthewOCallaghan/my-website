5dd012f62c264cee844a9d17904942f3fd3e08a5
Matthew O'Callaghan <matthewocallaghan@matthews-mini.home>
2021-03-20 22:39:31 +0000

    Removed potentially empty style tags from head of layout template

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,10 @@ function reload(cb) {
 
 function watchFiles() {
     watch('src/scss/**/*.scss', processSass);
-    watch(['src/pages/**/*.njk', 'src/templates/**/*.njk', 'src/data.json'], processNunjucks);
+    watch('src/templates/**/*.njk', processNunjucks);
+    watch('src/pages/**/*.njk', processNunjucks);
+    watch('src/data.json', processNunjucks);
+    // watch(['src/pages/**/*.njk', 'src/templates/**/*.njk', 'src/data.json'], processNunjucks);
     watch('src/js/**/*.js', reload);
 }
 
